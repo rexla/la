d23cde5ec3dca0a46372b34f8923f69993474ffd
rexla <45387759+rexla@users.noreply.github.com>
2022-02-25 10:04:08 +0800

    adds all files about react project1

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={require("../images/react-icon-small.png")} className="nav-icon" />
+            <h3 className="nav-icon-text">ReactFacts</h3>
+            <h4 className="nav-title">React Course - Project 1</h4>
+        </nav>
+    )
+}
